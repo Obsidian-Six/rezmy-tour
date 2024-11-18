26b0d87de18b09d0b585c1ed94bc1c1139425a5b
Aniket <aniketkalawat88@gmail.com>
2024-11-18 12:30:05 +0530

    google-tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/next-script-for-ga */
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "./_components/navbar";
@@ -6,6 +7,7 @@ import { Providers } from "./redux/provider";
 import FormPopup from "./_components/form-popup";
 import 'react-toastify/dist/ReactToastify.css';
 import Whatsapp from "./_components/Whatsapp";
+import Head from "next/head";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,23 +31,37 @@ export const metadata = {
 export default function RootLayoutFor({ children }) {
   return (
     <html lang="en">
+      <Head>
+        {/* Google Tag Manager Script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N98F7JV2');`,
+          }}
+        ></script>
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased relative`}
       >
+        {/* Google Tag Manager NoScript */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-N98F7JV2"
             height="0"
             width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
+            style={{ display: "none", visibility: "hidden" }}
           ></iframe>
         </noscript>
+
         <Providers>
-        <Navbar />
-        <FormPopup />
-        <Whatsapp />
-        {children}
-        <Footer />
+          <Navbar />
+          <FormPopup />
+          <Whatsapp />
+          {children}
+          <Footer />
         </Providers>
       </body>
     </html>
--- src/app/packages/page.jsx
+++ src/app/packages/page.jsx
@@ -1,45 +1,16 @@
-/* eslint-disable @next/next/next-script-for-ga */
-import Head from 'next/head';
-
 import React from 'react'
 import Navbar from '../_components/navbar'
-import NewPackages from '../_components/packages'
+import Packages from '../_components/packages'
 import PackageHero from '../_components/package-hero'
 
-export default function Packages() {
+const Pack = () => {
   return (
-    <>
-      <Head>
-        <title>Packages - Rezmytour</title>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-N98F7JV2');`,
-          }}
-        ></script>
-      </Head>
-      <div>
+    <div>
          <PackageHero name={"Packages"}  img={"/assets/images/hero-section/04.jpg"} page={"Packages"} />
-       <NewPackages />
-        {/* Your Packages page content */}
-      </div>
-    </>
-  );
+       <Packages />
+
+    </div>
+  )
 }
 
-
-
-
-
-// const Pack = () => {
-//   return (
-//     <div>
-
-//     </div>
-//   )
-// }
-
-// export default Pack
+export default Pack
