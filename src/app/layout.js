465b605a292b62ad7d9a5fd65fa21b08c7293555
Aniket <aniketkalawat88@gmail.com>
2024-11-19 13:09:54 +0530

    google-analytics-add

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,9 +5,10 @@ import Navbar from "./_components/navbar";
 import Footer from "./_components/footer";
 import { Providers } from "./redux/provider";
 import FormPopup from "./_components/form-popup";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import Whatsapp from "./_components/Whatsapp";
 import Head from "next/head";
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,45 +32,24 @@ export const metadata = {
 export default function RootLayoutFor({ children }) {
   return (
     <html lang="en">
-      <Head>
-        {/* Google Tag Manager Script */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-N98F7JV2');`,
-          }}
-        ></script>
-
-        {/* Google Tag (gtag.js) */}
-        <script
+      <head>
+        <Script
           async
           src="https://www.googletagmanager.com/gtag/js?id=AW-16778951876"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'AW-16778951876');`,
-          }}
-        ></script>
-      </Head>
+        ></Script>
+        <Script id="google-analytics">
+          {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+  
+    gtag('config', 'AW-16778951876');
+    `}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased relative`}
       >
-        {/* Google Tag Manager NoScript */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-N98F7JV2"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-
         <Providers>
           <Navbar />
           <FormPopup />
